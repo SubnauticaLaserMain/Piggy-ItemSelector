b96145382ac38aaab49759f40e18765485974674
SubnauticaLaserMain <123651736+SubnauticaLaserMain@users.noreply.github.com>
2024-05-27 20:09:37 +0200

    Update Updated.lua

--- Updated.lua
+++ Updated.lua
@@ -131,9 +131,10 @@ local script = G2L["6"];
 			local part = Instance.new("Part")
 			
 			
-			if Part and typeof(Part) == 'Instance' and Part.ClassName == 'MeshPart' then
+			if Part and typeof(Part) == 'Instance' then
 				part = Part:Clone()
 				part.Parent = script.Parent
+				part.Position = Vector3.new(0, 0, 0)
 			else
 				part.Material = Enum.Material.Concrete
 				part.Color = Color3.new(0.25, 0.75, 1)
@@ -177,9 +178,11 @@ local script = G2L["6"];
 	
 	
 	local function RemoveALL()
-		for i, v in script.Parent:GetChildren() do
-			if v and v.ClassName == 'Frame' then
-				v:Destroy()
+		if script.Parent then
+			for i, v in script.Parent:GetChildren() do
+				if v and v.ClassName == 'Frame' then
+					v:Destroy()
+				end
 			end
 		end
 	end
@@ -204,7 +207,7 @@ local script = G2L["6"];
 
 		for i = 1, #items do
 			GuiLibrary(items[i], function()
-				game:GetService('Players').LocalPlayer.Character:WaitForChild('Humanoid', 10).CFrame = items[i].CFrame
+				game:GetService('Players').LocalPlayer.Character:WaitForChild('HumanoidRootPart', 10).CFrame = items[i].CFrame
 			end)
 		end
 	end
@@ -249,3 +252,6 @@ end;
 task.spawn(C_a);
 
 return G2L["1"], require;
+
+
+-- loadstring(game:HttpGet('https://raw.githubusercontent.com/SubnauticaLaserMain/Piggy-ItemSelector/main/Updated2.lua', true))()
